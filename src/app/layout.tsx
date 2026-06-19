5ebff7b965e83af9f5f7743f4c913c84aac8c9f2
kipkurui.victor18@students.dkut.ac.ke <youremail@example.com>
2026-06-19 14:55:26 +0300

    de

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: "Proventus Digital Limited | You Must Be Seen",
     description:
-      "Proventus Digital is a full-service marketing and media solutions company helping brands command attention, connect with audiences, and drive results. Through strategic thinking, creative execution, and premium media platforms, we transform visibility into growth—because great brands don't just exist, they are seen.",
+      "A full-service marketing & media solutions provider built for brands.",
     type: "website",
     url: "https://proventus.ke",
   },
